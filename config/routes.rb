0d6fb0886f9365f6d2601d6bc28a7af2f51ffbeb
Rails udemy course <Railsudemycourse@gmail.com>
2020-11-25 23:55:26 +0530

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   get 'home', :to => 'home#search'
-  resources :registers do
-  collection do
-    post :create
-    end
-end
+  resources :registers
   root to: "students#index"
   resources :students
   resources :institutions
