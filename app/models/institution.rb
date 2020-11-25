5f158efb3fae01b75dc8c20592790e703d9cebbf
Rails udemy course <Railsudemycourse@gmail.com>
2020-11-26 00:48:00 +0530

    Sort feature

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,6 +2,6 @@ class Institution < ApplicationRecord
   has_many :students
 
   def self.search(name)
-    Institution.where('name like :value', value: "%#{name}%")
+    Institution.where('name like :value', value: "%#{name}%").sort_by { |i| i.name }
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,6 @@ class Student < ApplicationRecord
   belongs_to :institution
 
   def self.search(name)
-    Student.where('name like :value', value: "%#{name}%")
+    Student.where('name like :value', value: "%#{name}%").sort_by { |s| s.name }
   end
 end
